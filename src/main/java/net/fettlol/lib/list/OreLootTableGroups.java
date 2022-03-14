9b383a9cc6f076881c7380cbb7e4005e24497a72
Breki Tomasson <breki.tomasson@gmail.com>
2022-03-14 12:37:56 +0100

    Added Iron, Gold and Copper ores to the `OreLootTableGroups`
    Signed-off-by: Breki Tomasson <breki.tomasson@gmail.com>

--- src/main/java/net/fettlol/lib/list/OreLootTableGroups.java
+++ src/main/java/net/fettlol/lib/list/OreLootTableGroups.java
@@ -7,10 +7,34 @@ import java.util.List;
 
 public class OreLootTableGroups {
 
+    public static final List<String> IRON_ORES;
+    public static final List<String> GOLD_ORES;
+    public static final List<String> COPPER_ORES;
     public static final List<String> DIAMOND_ORES;
     public static final List<String> LAPIS_ORES;
 
     static {
+        IRON_ORES = Arrays.asList(
+            "minecraft:blocks/iron_ore",
+            "minecraft:blocks/deepslate_iron_ore",
+            "denseores:blocks/dense_deepslate_iron_ore",
+            "denseores:blocks/dense_iron_ore"
+        );
+
+        COPPER_ORES = Arrays.asList(
+            "minecraft:blocks/copper_ore",
+            "minecraft:blocks/deepslate_copper_ore",
+            "denseores:blocks/dense_deepslate_copper_ore",
+            "denseores:blocks/dense_copper_ore"
+        );
+
+        GOLD_ORES = Arrays.asList(
+            "minecraft:blocks/gold_ore",
+            "minecraft:blocks/deepslate_gold_ore",
+            "denseores:blocks/dense_deepslate_gold_ore",
+            "denseores:blocks/dense_gold_ore"
+        );
+
         DIAMOND_ORES = Arrays.asList(
             "minecraft:blocks/diamond_ore",
             "minecraft:blocks/deepslate_diamond_ore",
